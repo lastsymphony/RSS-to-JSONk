502199fd83b78e2e25cb351249e8a959a2717938
lastsymphony <lastsymphony01@gmail.com>
2025-08-02 21:02:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-var express = require('express');
-var app = express();
-var Feed = require('rss-to-json');
+const Feed = require('rss-to-json');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(req, res, next) {
-  if (req.query.feedURL) {
-    Feed.load(req.query.feedURL, function(err, rss){
-      if (err) {
-        res.send({ 'error': 'An error has occurred' });
-      } else {
-        res.send(rss);
-      }
-    });
-  } else {
-    res.status(400).send({ 'error': 'feedURL is required' });
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Only GET allowed' });
   }
-});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+  const { feedURL } = req.query;
+
+  if (!feedURL) {
+    return res.status(400).json({ error: 'feedURL is required' });
+  }
+
+  try {
+    const rss = await Feed.load(feedURL);
+    res.status(200).json(rss);
+  } catch (err) {
+    res.status(500).json({ error: err.message || 'Failed to fetch RSS' });
+  }
+};
